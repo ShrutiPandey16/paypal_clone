5b14786e21683b3bdc08ef75c2d994dda23a3504
shrutiPandey <shrutipandey23456@gmail.com>
2026-02-02 22:41:34 +0530

    Create TransactionController class

---
+++ transaction-service/src/main/java/com/paypal/transaction_service/controller/TransactionController.java
@@ -0,0 +1,33 @@
+package com.paypal.transaction_service.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.paypal.transaction_service.entity.Transaction;
+import com.paypal.transaction_service.service.TransactionService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/transaction")
+public class TransactionController {
+
+	private final TransactionService service;
+	
+	public TransactionController(TransactionService service) {
+		this.service = service;
+	}
+	
+	public ResponseEntity<?> create(@Valid @RequestBody Transaction transaction){
+		Transaction created = service.createTransaction(transaction);
+		return ResponseEntity.ok(created);
+	}
+	
+	public List<Transaction> getAll(){
+		return service.getAllTransactions();
+	}
+}
